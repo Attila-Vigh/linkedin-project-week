da251121ffab9b2b5fc997ed7c1c37d0915229ea
attila <attilavigh.contact@gmail.com>
2021-07-21 20:12:57 +0100

    App.jsx reorganised.

--- src/App.js
+++ src/App.js
@@ -1,17 +1,44 @@
 
 import './App.css';
-import Footer from './components/Footer';
-import TopNavbar from './components/Navbar/TopNavbar';
-import ProfilePage from './components/ProfilePage';
+import Aside                 from './components/Body/Aside/Aside';
+import Body                  from './components/Body/Body';
+import Footer                from './components/Footer';
+import Header                from './components/Body/Main/Header/Header';
+import Main                  from './components/Body/Main/Main';
+import AsideUserList         from './components/Body/Aside/AsideUserList/AsideUserList';
+import SidebarOptionalMenue  from './components/Body/Aside/SidebarOptionalMenue';
+import SkillsAndEndorsements from './components/Body/Main/SkillsAndEndorsements/SkillsAndEndorsements';
+import Experiences from './components/Body/Main/Experiences/Experiences';
+import TopNavbar             from './components/Navbar/TopNavbar';
 
-
-
-function App() {
+function App () {
     return (
         <div className="App">
-            <TopNavbar/>
-            <ProfilePage/>
-            <Footer/>
+
+            <TopNavbar />
+            <Body>
+                <Main>
+
+                    <Header />
+                    <SkillsAndEndorsements 
+                        title='Skills & endorsements' 
+                        crud ='edit' 
+                        add  ='Add a new skill'
+                    />
+                    <Experiences 
+                        title='Experiences'
+                        crud ='add'
+                    />
+                    
+                </Main>
+
+                <Aside>
+                    <SidebarOptionalMenue />
+                    <AsideUserList title='People also viewed' />
+                    <AsideUserList title='People you may know' />
+                </Aside>
+            </Body>
+            <Footer />
         </div>
     );
 
