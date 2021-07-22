250550b93a3de6ff9d428b1ce73ff87bc41f38ca
attila <attilavigh.contact@gmail.com>
2021-07-23 00:23:55 +0100

    HomePage component refactored.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -28,7 +28,7 @@ import CheckForErrorAndPending from '../../util/CheckForErrorAndPending';
 const Header = ({  }) => {
 
     // const { dataList: userList, isPending, isError } = useFetch( URL, AUTHORIZATION )
-    const URL = 'https://striveschool-api.herokuapp.com/api/profileError'
+    const URL = 'https://striveschool-api.herokuapp.com/api/profile'
     const { dataList: userList, isPending, isError } = useFetch( URL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2M4NWNlYWY0ODAwMTVjOTE4NjgiLCJpYXQiOjE2MjY3MDEzNzAsImV4cCI6MTYyNzkxMDk3MH0.IM9cEo_PuSRIB7l1erCyKvf0jtzAUGi2Vr_ARs71CME' )
 
     return (
--- src/components/NewsFeed/HomePage.jsx
+++ src/components/NewsFeed/HomePage.jsx
@@ -6,35 +6,14 @@ import ProfileBox from '../ProfileBox'
 import NewsFeedPoster from '../NewsFeed/NewsFeedPostInput/Home-Top'
 import HomeAsideNewsAndTopCourses from './HomeAsideNewsAndTopCourses/HomeAsideNewsAndTopCourses';
 import HomeAsideItem from './HomeAsideNewsAndTopCourses/HomeAsideItem';
+import useFetch from '../../util/useFetch';
+import CheckForErrorAndPending from '../../util/CheckForErrorAndPending';
 
 
 export default function HomePage() {
 
-    const [posts, setPosts] = useState([])
-
-    useEffect(() => {
-        fetchPosts()
-        console.log('this is it',posts);
-        
-    }, [])
-
-
-    const fetchPosts = async () => {
-        try {
-            const response = await fetch('https://striveschool-api.herokuapp.com/api/posts/', {
-                headers: { 'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2M4NWNlYWY0ODAwMTVjOTE4NjgiLCJpYXQiOjE2MjY3MDEzNzAsImV4cCI6MTYyNzkxMDk3MH0.IM9cEo_PuSRIB7l1erCyKvf0jtzAUGi2Vr_ARs71CME` }
-            })
-            if (response.ok) {
-                const data = await response.json()
-                console.log(data);
-                setPosts(data)
-            }
-
-        } catch (error) {
-
-        }
-    }
-
+    const URL = 'https://striveschool-api.herokuapp.com/api/posts/'
+    const { dataList: posts, isPending, isError } = useFetch( URL, 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJfaWQiOiI2MGFlM2M4NWNlYWY0ODAwMTVjOTE4NjgiLCJpYXQiOjE2MjY3MDEzNzAsImV4cCI6MTYyNzkxMDk3MH0.IM9cEo_PuSRIB7l1erCyKvf0jtzAUGi2Vr_ARs71CME' )
 
     return (
         <Container id='profilepage' className='mt-1'>
@@ -44,6 +23,7 @@ export default function HomePage() {
                 </Col>
                 <Col className='col-md-6 ordder-md-2 px-3 pl-4 '>
                     <NewsFeedPoster/>
+                    <CheckForErrorAndPending isPending={ isPending } isError={ isError } />
                     {
                         posts && 
                         posts.slice(0, 5).map( post => 
